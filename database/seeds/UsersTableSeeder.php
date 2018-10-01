45828d7e6990ae63882e19a5caba19462270aafb
Rodrigo Tmontec <rodrigo.cursino@tmontec.com.br>
2018-10-01 14:06:39 -0300

    primeiro commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createAdmin();
+    }
+
+    private function createAdmin ()
+    {
+        User::create([
+            'name'     => 'Admin',
+            'email'    => 'admin@admin.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
